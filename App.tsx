1b6a6044bae165b99029e7f07d82f7efd5d427a8
papercoding22 <john.ly997@gmail.com>
2024-10-31 15:06:15 +0700

    basic setup for react-native-gesture-handler

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import RootNavigator from './src/navigation/RootNavigator';
 
-import "./global.css"
+import './global.css';
+import TestGestureScreen from './src/screens/TestGestureScreen';
 
 function App(): React.JSX.Element {
-  return <RootNavigator />;
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      {/* <RootNavigator /> */}
+      <TestGestureScreen />
+    </GestureHandlerRootView>
+  );
 }
 
 export default App;
